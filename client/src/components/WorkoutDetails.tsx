f2ffc0a7f26e98a14c5b5cf17af66c3d65b56e15
Lester Cuadra <lester.cuadra24@gmail.com>
2023-03-14 18:20:28 +0800

    feat: fetch and show all workouts

---
+++ client/src/components/WorkoutDetails.tsx
@@ -0,0 +1,29 @@
+import { Workout } from "../Types";
+
+interface WorkoutDetailsProps {
+  key: string;
+  workout: Workout;
+}
+
+const WorkoutDetails = (props: WorkoutDetailsProps) => {
+  const { workout } = props;
+
+  return (
+    <div className="workout-details">
+      <h1>{workout.title}</h1>
+      <p>
+        <strong>ID:</strong> {workout._id}
+      </p>
+      <p>
+        <strong>Load (kg):</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt.toLocaleString()}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import { Workout } from "../Types";
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [workouts, setWorkouts] = useState<Array<Workout> | any>(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:5000/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout: Workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
