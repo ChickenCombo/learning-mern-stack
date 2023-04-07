2cadcad3efa2a1409823a455c0ee765e0fafd0ca
Lester Cuadra <lester.cuadra24@gmail.com>
2023-04-07 17:53:58 +0800

    feat: sign jwt token

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+
+const createToken = (_id: string) => {
+  return jwt.sign({ _id }, process.env.SECRET_KEY!, { expiresIn: '3d'});
+}
 
 export const loginUser = async (req: Request, res: Response) => {
   res.json({ message: 'User logged in' })
@@ -11,7 +17,9 @@ export const signupUser = async (req: Request, res: Response) => {
   try {
     const user = await User.signUp(email, password);
 
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : 'An error occurred while signing up the user.';
     res.status(400).json({ error: errorMessage });
